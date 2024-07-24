704c7ae392b61395ac566a490d8e9e0cffbae20f
Leyuur <yuri.vduarte@hotmail.com>
2024-07-24 19:00:50 -0300

    testes

--- js/app.js
+++ js/app.js
@@ -1,35 +1,31 @@
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.4/firebase-app.js";
+import { GoogleAuthProvider, getAuth, signInWithPopup } from "https://www.gstatic.com/firebasejs/10.12.4/firebase-auth.js";
+import { getAnalytics } from "https://www.gstatic.com/firebasejs/10.12.4/firebase-analytics.js";
 
-  import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.4/firebase-app.js";
-  import { GoogleAuthProvider, getAuth, signInWithPopup } from "https://www.gstatic.com/firebasejs/10.12.4/firebase-auth.js";
-  import { getAnalytics } from "https://www.gstatic.com/firebasejs/10.12.4/firebase-analytics.js";
+const firebaseConfig = {
+  apiKey: "AIzaSyAObRm90Wps9GkcskYq1GgZJBDv3jJAq2s",
+  authDomain: "atlas-14386.firebaseapp.com",
+  projectId: "atlas-14386",
+  storageBucket: "atlas-14386.appspot.com",
+  messagingSenderId: "77319468044",
+  appId: "1:77319468044:web:e31abcf323b0d85d566a29",
+  measurementId: "G-TN95H2W4JK"
+};
 
-  const firebaseConfig = {
-    apiKey: "AIzaSyAObRm90Wps9GkcskYq1GgZJBDv3jJAq2s",
-    authDomain: "atlas-14386.firebaseapp.com",
-    projectId: "atlas-14386",
-    storageBucket: "atlas-14386.appspot.com",
-    messagingSenderId: "77319468044",
-    appId: "1:77319468044:web:e31abcf323b0d85d566a29",
-    measurementId: "G-TN95H2W4JK"
-  };
-
-  // Initialize Firebase
-  const app = initializeApp(firebaseConfig);
-  const analytics = getAnalytics(app);
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
 
 const provider = new GoogleAuthProvider();
 const auth = getAuth();
 
-document.getElementById('login-btn').addEventListener('click', ()=>{
-    signInWithPopup(auth, provider)
+document.getElementById('login-btn').addEventListener('click', () => {
+  signInWithPopup(auth, provider)
     .then((result) => {
-
-    // This gives you a Google Access Token. You can use it to access the Google API.
-
       const credential = GoogleAuthProvider.credentialFromResult(result);
       const token = credential.accessToken;
       const user = result.user;
-      
+
       if (user.email.includes("icone.g12.br")) {
         Toastify({
           text: "Logado com sucesso",
@@ -44,13 +40,12 @@ document.getElementById('login-btn').addEventListener('click', ()=>{
           onClick: function(){} // Callback after click
         }).showToast();
 
-        console.log(user);
-
+        // Armazena as informações do usuário no localStorage
         localStorage.setItem('user', JSON.stringify(user));
 
-        window.location="https://circle-atlas.github.io/atlas-WEB/menu-coordenador.html";
-      }
-      else {
+        // Redireciona para a nova página
+        window.location = "https://circle-atlas.github.io/atlas-WEB/menu-coordenador.html";
+      } else {
         Toastify({
           text: "Login inválido",
           duration: 3000,
@@ -64,15 +59,12 @@ document.getElementById('login-btn').addEventListener('click', ()=>{
           onClick: function(){} // Callback after click
         }).showToast();
       }
-
     }).catch((error) => {
       // Handle Errors here.
       const errorCode = error.code;
       const errorMessage = error.message;
-      // The email of the user's account used.
       const email = error.customData.email;
-      // The AuthCredential type that was used.
       const credential = GoogleAuthProvider.credentialFromError(error);
       // ...
     });
-})
+});
