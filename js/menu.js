5461e361eec3e6a7c840d889dd5cd668f7616359
Leyuur <yuri.vduarte@hotmail.com>
2024-07-24 19:16:39 -0300

    adicionei teste de foto de perfil

--- js/menu.js
+++ js/menu.js
@@ -7,6 +7,8 @@ console.log(userData);
 
 document.getElementById('bem-vindo').innerHTML = `Bem-vindo, ${userData.displayName}`;
 
+document.getElementById('foto-perfil').src = userData.photoURL;
+
 } else {
 window.location = "https://circle-atlas.github.io/atlas-WEB/login.html";
 }
