2607bc583abe524d6d7dc6cd3e5d84d1669f21a6
Leyuur <yuri.vduarte@hotmail.com>
2024-08-15 21:22:36 -0300

    mexi no front

--- js/stats.js
+++ js/stats.js
@@ -1,6 +1,6 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.13.0/firebase-app.js";
-import { getFirestore, collection, getDocs, updateDoc, doc, query, where,} from "https://www.gstatic.com/firebasejs/10.13.0/firebase-firestore.js";
+import { getFirestore, collection, getDocs, updateDoc, doc, query, where, orderBy} from "https://www.gstatic.com/firebasejs/10.13.0/firebase-firestore.js";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
@@ -17,7 +17,7 @@ const firebaseConfig = {
 const app = initializeApp(firebaseConfig);
 const db = getFirestore(app);
 
-const q = query(collection(db, "users"));
+const q = query(collection(db, "users"), orderBy(admin.firestore.FieldPath.documentId(),"desc"));
 
 const alunos = await getDocs(q);
 
