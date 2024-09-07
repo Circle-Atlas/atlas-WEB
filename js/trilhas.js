50f2d6e6205e585480e11675d923a13a9223952e
Yuri <yuri.vduarte@hotmail.com>
2024-09-07 11:49:05 -0300

    adicione um registrador das mudanças na trilha

--- js/trilhas.js
+++ js/trilhas.js
@@ -2,6 +2,8 @@ import { initializeApp } from "https://www.gstatic.com/firebasejs/10.13.0/fireba
 import { getFirestore } from "https://www.gstatic.com/firebasejs/10.13.0/firebase-firestore.js";
 import { firebaseConfig, user, sair } from 'https://circle-atlas.github.io/atlas-WEB/js/config.js';
 
+
+
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 const db = getFirestore(app);
@@ -18,64 +20,75 @@ document.getElementById('nav-sair').addEventListener('click', (e) => {
     e.preventDefault();
     sair();
 });
-
-try {
-    const trilhaContainer = document.querySelector('.trilha');
-
-    dadosMateria.nomeColecoes.forEach(colecao => {
-        let colecaoContainer = document.createElement('div');
-        colecaoContainer.className = 'colecao-container';
-        colecaoContainer.draggable = true;
-        colecaoContainer.innerText = colecao;
-
-        trilhaContainer.append(colecaoContainer);
-    });
-
-    document.addEventListener('dragstart', (e) => {
-        e.target.classList.add("dragging");
-    });
-
-    document.addEventListener('dragend', (e) => {
-        e.target.classList.remove("dragging");
-    });
-
-    trilhaContainer.addEventListener("dragover", (e) => {
-        e.preventDefault();
-        const dragging = document.querySelector(".dragging");
-        const afterElement = getDragAfterElement(trilhaContainer, e.clientY);
-        if (afterElement == null) {
-            trilhaContainer.appendChild(dragging);
-        } else {
-            trilhaContainer.insertBefore(dragging, afterElement);
-        }
-    });
-
-    function getDragAfterElement(container, y) {
-        const draggableElements = [...container.querySelectorAll('.colecao-container:not(.dragging)')];
-
-        return draggableElements.reduce((closest, child) => {
-            const box = child.getBoundingClientRect();
-            const offset = y - box.top - box.height / 2;
-            if (offset < 0 && offset > closest.offset) {
-                return { offset: offset, element: child };
-            } else {
-                return closest;
-            }
-        }, { offset: Number.NEGATIVE_INFINITY }).element;
+if (!document.querySelector('.colecao-container')) {
+    try {
+        const trilhaContainer = document.querySelector('.trilha');
+    
+        dadosMateria.nomeColecoes.forEach(colecao => {
+            let colecaoContainer = document.createElement('div');
+            colecaoContainer.className = 'colecao-container';
+            colecaoContainer.draggable = true;
+            colecaoContainer.innerText = colecao;
+    
+            trilhaContainer.append(colecaoContainer);
+        });
+    
+    } catch (error) {
+        document.querySelector('.erro').hidden = false;
+        Toastify({
+            text: "Erro ao carregar as coleções",
+            duration: 6000,
+            close: true,
+            gravity: "top", 
+            position: "center",
+            stopOnFocus: true,
+            style: {
+                background: "red",
+            },
+        }).showToast();
+        console.error("Erro: ", error);
     }
+}
 
-} catch (error) {
-    document.querySelector('.erro').hidden = false;
-    Toastify({
-        text: "Erro ao carregar as coleções",
-        duration: 6000,
-        close: true,
-        gravity: "top", 
-        position: "center",
-        stopOnFocus: true,
-        style: {
-            background: "red",
-        },
-    }).showToast();
-    console.error("Erro: ", error);
+const trilhaContainer = document.querySelector('.trilha');
+
+document.addEventListener('dragstart', (e) => {
+    e.target.classList.add("dragging");
+});
+
+document.addEventListener('dragend', (e) => {
+    e.target.classList.remove("dragging");
+    let colecoes = document.querySelectorAll('.colecao-container')
+    let count = 0;
+    let insert = [];
+    colecoes.forEach(colecao =>{
+        insert[count] = colecao.innerHTML;
+        count = ++count;
+    })
+    console.log(insert);
+});
+
+trilhaContainer.addEventListener("dragover", (e) => {
+    e.preventDefault();
+    const dragging = document.querySelector(".dragging");
+    const afterElement = getDragAfterElement(trilhaContainer, e.clientY);
+    if (afterElement == null) {
+        trilhaContainer.appendChild(dragging);
+    } else {
+        trilhaContainer.insertBefore(dragging, afterElement);
+    }
+});
+
+function getDragAfterElement(container, y) {
+    const draggableElements = [...container.querySelectorAll('.colecao-container:not(.dragging)')];
+
+    return draggableElements.reduce((closest, child) => {
+        const box = child.getBoundingClientRect();
+        const offset = y - box.top - box.height / 2;
+        if (offset < 0 && offset > closest.offset) {
+            return { offset: offset, element: child };
+        } else {
+            return closest;
+        }
+    }, { offset: Number.NEGATIVE_INFINITY }).element;
 }
